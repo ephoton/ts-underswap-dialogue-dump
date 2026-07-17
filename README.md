39eeae423d5f3291e1c56e9997d49c9f27e84374
photon <286164830+ephoton@users.noreply.github.com>
2026-07-17 19:19:22 -0400

    feat: Initial
    Signed-off-by: photon <286164830+ephoton@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+# TS!Underswap dialogue dump
+
+How to use:
+
+1. Install I'm an issue's [DiannexParser](https://github.com/BenjaminUrquhart/DiannexParser) and extract assembly to the `diannex/` folder.
+2. `cargo run`
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// Copyright (C) 2026 photon.
+//
+// This file is part of tsus_dialoguedump.
+//
+// tsus_dialoguedump is free software: you can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation, version 3 of the License
+// only.
+//
+// tsus_dialoguedump is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with tsus_dialoguedump. If not, see
+// <https://www.gnu.org/licenses/>.
+
+fn main() {
+    println!("Hello, world!");
+}
